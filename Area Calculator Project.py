569e0478dea2a0707de279b4669b88c9fe39d804
PhattharapornTantawan <149596492+PhattharapornTantawan@users.noreply.github.com>
2023-11-08 19:56:46 +0700

    Area Calculator Project.py
    Let's play

--- Area Calculator Project.py
+++ Area Calculator Project.py
@@ -1,32 +1,68 @@
 import sys
 from tkinter import *
+from tkinter import simpledialog, messagebox
 
+# Function to calculate the area
 def calculate_area():
-    width = float(width_entry.get())
-    height = float(height_entry.get())
-    total = width * height
-    result_label.configure(text=f'Area of rectangle is = {total:.2f}')
+    try:
+        width = float(width_entry.get())
+        height = float(height_entry.get())
+        
+        # Calculate the area
+        total = width * height
+        
+        # Display the result in a message box
+        result = f'Area of the rectangle is {total:.2f}'
+        messagebox.showinfo('Result', result)
+        
+        # Check if the user wants to continue
+        answer = simpledialog.askstring('Continue', 'Do you want to try again? (y/n): ')
+        
+        if answer == 'n':
+            messagebox.showinfo('Thanks', 'Thank you for playing!')
+            root.destroy()
+            sys.exit()
+    except ValueError:
+        # Handle invalid input
+        result_label.configure(text='Invalid input. Please enter valid numbers.')
 
+# Function for the "OK" button
+def ok():
+    # Implement the desired behavior when the "OK" button is clicked
+    result_label.configure(text='')
+
+# Function for the "Cancel" button
+def cancel():
+    # Implement the desired behavior when the "Cancel" button is clicked
+    width_entry.delete(0, END)
+    height_entry.delete(0, END)
+
+# Create the main application window
 root = Tk()
 root.title("Area Calculator")
 root.geometry('350x200')
 
-width_label = Label(root, text="Enter width:")
-width_label.grid(row=0, column=0)
-
+# Create labels and entry fields for width and height
+width_label = Label(root, text="Width:")
+width_label.pack()
 width_entry = Entry(root)
-width_entry.grid(row=0, column=1)
+width_entry.pack()
 
-height_label = Label(root, text="Enter height:")
-height_label.grid(row=1, column=0)
-
+height_label = Label(root, text="Height:")
+height_label.pack()
 height_entry = Entry(root)
-height_entry.grid(row=1, column=1)
+height_entry.pack()
 
-calculate_button = Button(root, text="Calculate Area", command=calculate_area)
-calculate_button.grid(row=2, column=0, columnspan=2)
+# Create "OK" and "Cancel" buttons
+calculate_button = Button(root, text="OK", command=calculate_area)
+calculate_button.pack()
 
+cancel_button = Button(root, text="Cancel", command=cancel)
+cancel_button.pack()
+
+# Create a label to display the result
 result_label = Label(root, text="")
-result_label.grid(row=3, column=0, columnspan=2)
+result_label.pack()
 
+# Start the main event loop
 root.mainloop()
